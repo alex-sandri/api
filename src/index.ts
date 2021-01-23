d52de32831ab1bf6d4bb41cb6d3a2e6da16de1b8
Alex Sandri <me@alexsandri.com>
2021-01-23 09:05:58 +0100

    Create Api class

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,62 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const app = express();
+interface ApiConfig
+{
+    port?: number;
+    endpoints: Endpoint[];
+}
 
-app.use(cors());
-app.use(helmet());
-app.use(bearerToken());
+interface Endpoint
+{
+    url: string;
+    method: "DELETE" | "GET" | "POST" | "PUT";
+    callback: (body: any) => Promise<void>;
+}
 
-app.use(express.json());
+export default class Api
+{
+    public constructor(config: ApiConfig)
+    {
+        const app = express();
 
+        app.use(cors());
+        app.use(helmet());
+        app.use(bearerToken());
 
+        app.use(express.json());
 
-app.listen(3000);
+        config.endpoints.forEach(endpoint =>
+        {
+            switch (endpoint.method)
+            {
+                case "DELETE":
+                {
+                    app.delete(endpoint.url, endpoint.callback);
+
+                    break;
+                }
+                case "GET":
+                {
+                    app.get(endpoint.url, endpoint.callback);
+
+                    break;
+                }
+                case "POST":
+                {
+                    app.post(endpoint.url, endpoint.callback);
+
+                    break;
+                }
+                case "PUT":
+                {
+                    app.put(endpoint.url, endpoint.callback);
+
+                    break;
+                }
+            }
+        });
+
+        app.listen(config.port ?? 3000);
+    }
+}
