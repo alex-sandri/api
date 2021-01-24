bd41996a34c7f33af1756884aacbb1128d9cae52
Alex Sandri <me@alexsandri.com>
2021-01-24 19:56:38 +0100

    Fix: must catch errors also in Endpoint class

--- src/models/Endpoint.ts
+++ src/models/Endpoint.ts
@@ -26,7 +26,19 @@ export class Endpoint
     {
         const response = Response.from(res);
 
-        this.config.callback(req, response);
+        try
+        {
+            await this.config.callback(req, response);
+        }
+        catch (error)
+        {
+            if (error instanceof Error)
+            {
+                // TODO: Create ApiError class to replace the generic Error
+                // TODO: id must be in the format aaa/bbb/ccc, example: user/password/wrong
+                response.body.errors = [ { id: "error", message: error.message } ];
+            }
+        }
     }
 }
 
